d2aa03f5d91b70af2a96d1e6be1e69a4ca919be4
StuartFletcher1990 <stuart.fletcher@iceland.co.uk>
2023-11-29 14:44:50 +0000

    updated

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module young
+module youngling
 
 go 1.21.0
--- youngling.go
+++ youngling.go
@@ -1,8 +1,8 @@
-package young
+package youngling
 
 import "fmt"
 
 func Youngling()  {
 	fmt.Println("There are too many of them, what are we going to do?")
-	
+
 }
