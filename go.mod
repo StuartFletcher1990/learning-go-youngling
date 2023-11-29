1aac0d13ec6293a73707c9ba9d773e3f6475efe5
StuartFletcher1990 <stuart.fletcher@iceland.co.uk>
2023-11-29 12:04:51 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/StuartFletcher1990/learning-go-youngling
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("There are too many of them, what are we going to do?")
+}
